cc40e61c272ddba86891f31a2c584a0fed18ac67
mariojespada <46303116+mariojespada@users.noreply.github.com>
2021-10-28 11:03:27 +0200

    Agregar archivos de proyecto.

---
+++ RomanNumeralsKata/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RomanNumeralsKata // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Kata!");
+        }
+    }
+}
